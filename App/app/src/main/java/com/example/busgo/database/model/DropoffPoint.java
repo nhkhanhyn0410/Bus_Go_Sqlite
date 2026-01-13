29ee31be2f29a35404f100100727e1826140aaf2
Nguyen_Hong_Khanh <nhkyn.0410@gmail.com>
2026-01-13 16:37:31 +0700

    feat: add data models
    Adds the following data models:
    - Trip
    - PickupPoint
    - DropoffPoint

---
+++ App/app/src/main/java/com/example/busgo/database/model/DropoffPoint.java
@@ -0,0 +1,75 @@
+package com.example.busgo.database.model;
+
+public class DropoffPoint {
+    private int id;
+    private int routeId;
+    private String pointName;
+    private String address;
+    private int timeOffset;
+    private boolean isActive;
+    private String actualDropoffTime;
+
+    public DropoffPoint(int routeId, String pointName, String address, int timeOffset) {
+        this.routeId = routeId;
+        this.pointName = pointName;
+        this.address = address;
+        this.timeOffset = timeOffset;
+        this.isActive = true;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRouteId() {
+        return routeId;
+    }
+
+    public void setRouteId(int routeId) {
+        this.routeId = routeId;
+    }
+
+    public String getPointName() {
+        return pointName;
+    }
+
+    public void setPointName(String pointName) {
+        this.pointName = pointName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getTimeOffset() {
+        return timeOffset;
+    }
+
+    public void setTimeOffset(int timeOffset) {
+        this.timeOffset = timeOffset;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public String getActualDropoffTime() {
+        return actualDropoffTime;
+    }
+
+    public void setActualDropoffTime(String actualDropoffTime) {
+        this.actualDropoffTime = actualDropoffTime;
+    }
+}
