0e553fb7d95d5c3c822624b66a46c1f2f7737ec4
Hồ Nguyễn Phúc Thông <phucthong0412@gmail.com>
2026-01-15 14:56:59 +0700

    feat: add helo.java
    Adds a new java file `helo.java` which contains a main method to print "Hello, World!".

--- App/app/src/main/java/com/example/busgo/helo.java
+++ App/app/src/main/java/com/example/busgo/helo.java
@@ -2,6 +2,6 @@ package com.example.busgo;
 
 public class helo {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello");
     }
 }
