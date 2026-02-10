54a67e74adc80e1021ea6a68b5952c8d2421119a
Nguyen_Hong_Khanh <nhkyn.0410@gmail.com>
2026-02-10 13:10:54 +0700

    done SessionManager

--- App/app/src/main/java/com/example/busgo/until/PriceCalculator.java
+++ App/app/src/main/java/com/example/busgo/until/PriceCalculator.java
@@ -1,4 +1,16 @@
 package com.example.busgo.until;
 
+import java.text.DecimalFormat;
+
 public class PriceCalculator {
+    private static final DecimalFormat PRICE_FORMAT = new DecimalFormat("#,###");
+    public static double calculateTotalPrice(double basePrice, int numSeats) {
+        return basePrice * numSeats;
+    }
+    public static String formatPrice(double price) {
+        return PRICE_FORMAT.format(price) + "VNĐ";
+    }
+    public static String formatPriceNoUnit(double price) {
+        return PRICE_FORMAT.format(price);
+    }
 }
