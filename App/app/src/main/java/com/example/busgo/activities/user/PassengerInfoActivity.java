dacf637e1e36421662dd1df377e88ba5f1f650b8
Truong Nguyen <nguyenxuantruong08032005@gmail.com>
2026-02-24 12:10:32 +0700

    add BG-10-PHASE_10

--- App/app/src/main/java/com/example/busgo/activities/user/PassengerInfoActivity.java
+++ App/app/src/main/java/com/example/busgo/activities/user/PassengerInfoActivity.java
@@ -1,4 +1,17 @@
 package com.example.busgo.activities.user;
 
-public class PassengerInfoActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.busgo.R;
+
+public class PassengerInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passenger_info);
+    }
 }
+
