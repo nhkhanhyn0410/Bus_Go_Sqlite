912236efee40947a6a48f5f0cfc282010f320036
Nguyen_Hong_Khanh <nhkyn.0410@gmail.com>
2026-03-02 20:04:48 +0700

    .

---
+++ App/app/src/main/java/com/example/busgo/until/ExpandableGridView.java
@@ -0,0 +1,28 @@
+package com.example.busgo.until;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridView;
+
+public class ExpandableGridView extends GridView {
+
+    public ExpandableGridView(Context context) {
+        super(context);
+    }
+
+    public ExpandableGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ExpandableGridView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        // Cho phép GridView mở rộng hết chiều cao thay vì bị cắt
+        int expandSpec = MeasureSpec.makeMeasureSpec(
+                Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+    }
+}
